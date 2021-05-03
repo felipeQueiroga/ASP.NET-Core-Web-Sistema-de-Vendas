b9b3a29c73fb43baf3ebbed1e7461259b90081b4
Felipe Queiroga <luisfelipequeiroga@hotmail.com>
2021-05-03 17:58:04 -0400

    @model IEnumerable<IGrouping<Department, SalesRecord>> grouping search

--- SalesWebMvc/Services/SalesRecordService.cs
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -37,7 +37,9 @@ namespace SalesWebMvc.Services
 
         public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
         {
+
             var result = from obj in _context.SalesRecord select obj;
+
             if (minDate.HasValue)
             {
                 result = result.Where(x => x.Date >= minDate.Value);
@@ -46,12 +48,14 @@ namespace SalesWebMvc.Services
             {
                 result = result.Where(x => x.Date <= maxDate.Value);
             }
-            return await result
-                .Include(x => x.Seller)
-                .Include(x => x.Seller.Department)
-                .OrderByDescending(x => x.Date)
-                .GroupBy(x => x.Seller.Department)
-                .ToListAsync();
+
+            var data = await result
+                 .Include(s => s.Seller)
+                 .Include(s => s.Seller.Department)
+                 .OrderByDescending(s => s.Date)
+                 .ToListAsync();
+
+            return data.GroupBy(s => s.Seller.Department).ToList();
         }
     }
 }
